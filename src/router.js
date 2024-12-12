d805608316f234a89f62fc07554ed80a2d067802
Egor M <egor.m@agiliumlabs.io>
2024-12-12 07:48:04 +0300

    Add home page

--- src/router.js
+++ src/router.js
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from "./views/Home.vue";
+import Dashboard from "./views/Dashboard.vue";
 
 const routes = [
     { path: '/', component: Home },
+    { path: '/dashboard', component: Dashboard },
 ];
 
 const router = createRouter({
